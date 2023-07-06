24348d8f36f36c7d87834987594a37864dce347f
naveen p <naveenpendekatla0407@gmail.com>
2023-07-06 18:57:28 +0530

    ansible frontend

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "aws_ssm_parameter" "parameters" {
   count = length(var.parameters)
   name  = var.parameters[count.index].name
   value  = var.parameters[count.index].value
-  type = "string"
+  type = "String"
   key_id = "4cc4aa7a-e873-409c-bbe8-fc4b8a80bc2c"
 }
